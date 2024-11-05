b7b363a9dc538644520d91f20963b9688c65ffe7
mahdadi <mahdadihc@gmail.com>
2024-11-05 13:03:54 +0330

    //todo Always portrate Orientation

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:sample1_youtube/home_page.dart';
 
 void main() {
+  //todo Always portrate Orientation
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
+
   runApp(const MyApp());
 }
 
