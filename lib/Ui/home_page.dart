76a8069d28806076e6ae370bdf9dd4d3bd0d7160
mahdadi <mahdadihc@gmail.com>
2024-11-06 20:55:01 +0330

    Internationalizing Flutter apps

--- lib/Ui/home_page.dart
+++ lib/Ui/home_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:provider_base_tools/provider_base_tools.dart';
+import 'package:sample1_youtube/Provider/theme_provider.dart';
 import 'package:sample1_youtube/Ui/ui_helper/theme_changer_buttum.dart';
 
 class MyHomePage extends StatefulWidget {
@@ -11,11 +13,19 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
+    final themeP = Provider.of<ThemeProvider>(context);
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: const Text('youtube'),
         actions: const [ThemeChanger()],
+      ),
+      body: Center(
+        child: Text(
+          'hello world',
+          style: TextStyle(
+              color: themeP.isDarkMode() ? Colors.white : Colors.black54),
+        ),
       ),
     );
   }
